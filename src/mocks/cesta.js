be84e4d62dc476eec39739fd6059a2615791d5b0
Felipe Wolff <wolff.felipe@hotmail.com>
2022-02-15 23:08:44 -0300

    Commit from ipad

--- src/mocks/cesta.js
+++ src/mocks/cesta.js
@@ -15,7 +15,7 @@ const cesta = {
     nome: "Cesta de Verduras",
     logoFazenda: logo,
     nomeFazenda: "Jenny Jack Farm",
-    descricao: "Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha",
+    descricao: "Cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha",
     preco: "R$ 40,00",
     botao: "Comprar",
   },
